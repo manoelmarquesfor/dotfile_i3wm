1cc3483896dd805fd0d6501493e606ebecb8b5a4
Manoel <manoeljuniorsb@hotmail.com>
2025-03-08 23:46:04 -0300

    atualizando lista de aplicativos e melhorando a instalação no script

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ APP_LIST="apps.txt"
 while IFS= read -r line; do
     # Ignora linhas vazias e comentários
     if [[ ! -z "$line" && ! "$line" =~ ^# ]]; then
-        echo "Instalando: $line"
-        sudo apt install -y "$line" 
+        trimmed_line=$(echo "$line" | sed 's/^[ \t]*//;s/[ \t]*$//')
+        
+        echo "Instalando: $trimmed_line"
+        sudo apt install -y "$trimmed_line"
     fi
 done < "$APP_LIST"
