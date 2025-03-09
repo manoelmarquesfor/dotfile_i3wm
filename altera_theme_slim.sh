16c7a1594076de3daefd778de6fdd74751b7490f
Manoel <manoeljuniorsb@hotmail.com>
2025-03-08 23:27:03 -0300

    corrigindo caminho do tema e melhorando a cópia do tema para o SLiM

--- altera_theme_slim.sh
+++ altera_theme_slim.sh
@@ -2,12 +2,12 @@
 
 THEME="minimal"
 PATH_THEMES_SLIM="/usr/share/slim/themes"
-PATH_THEME="$pwd/themes/slim/$THEME"
+PATH_THEME="./themes/slim/$THEME"
 
 PATH_CONFIG_SLIM="/etc/slim.conf"
 
 echo "Tema copiado para $PATH_THEMES"
-cp "$PATH_THEME" "$PATH_THEMES_SLIM"
+cp -r "$PATH_THEME" "$PATH_THEMES_SLIM"
 echo "Tema copiado com sucesso."
 
 
@@ -16,6 +16,6 @@ echo "Configurando o tema no arquivo de configuração do SLiM..."
 NOVO_THEME="current_theme     $THEME"
 
 # Usando sed para substituir toda a linha que contém 'current_theme'
-sed -i "/current_theme/c\\$NOVO_TEXTO" "$PATH_CONFIG_SLIM"
+sed -i "/current_theme/c\\$NOVO_THEME" "$PATH_CONFIG_SLIM"
 
 echo "Theme alterado com sucesso."
